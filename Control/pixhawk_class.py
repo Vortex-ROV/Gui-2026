9f31ebeb733d8f80dd96fd2da3758428e5bfb316
Mohamed Ashraf <mtaherr@proton.me>
2026-06-12 01:06:15 +0300

    separated ramp up and ramp down values

--- Control/pixhawk_class.py
+++ Control/pixhawk_class.py
@@ -71,7 +71,7 @@ class Pixhawk(QThread):
         self.__current_yaw_value = 1500
         self.__current_forward_value = 1500
         self.__current_lateral_value = 1500
-        self.__armed = False
+        self.armed = False
         self.__last_time_seen = 0
         self.__connected = False
         self.__pwm_value_range = 400
@@ -82,7 +82,8 @@ class Pixhawk(QThread):
         self.__rov_flip_lateral = 1
         self.__rov_flip_forward = 1
     
-        self.__change = 8
+        self.__change_up = 5
+        self.__change_down = 100
         self.disarm_procedure = False
 
     def get_gain(self): return self.__gain
@@ -148,10 +149,10 @@ class Pixhawk(QThread):
 
     def control_arm_disarm(self):
         if self.__connected:
-            self.__pixhawk.arducopter_disarm() if self.__armed else self.__pixhawk.arducopter_arm()
+            self.__pixhawk.arducopter_disarm() if self.armed else self.__pixhawk.arducopter_arm()
 
     def control_arm(self):
-        self.__pixhawk.arducopter_arm() if self.__armed == 0 else print("pixhawk already armed")
+        self.__pixhawk.arducopter_arm() if self.armed == 0 else print("pixhawk already armed")
 
     def control_disarm(self):
         if self.connected:
@@ -201,11 +202,11 @@ class Pixhawk(QThread):
                 if msg:
                     if self.disarm_procedure:
                         self.reset_rov_movements()
-                        if ((self.__current_roll_value <= self.__roll_value + self.__change and self.__current_roll_value >= self.__roll_value - self.__change)
-                        and (self.__current_throttle_value <= self.__throttle_value + self.__change and self.__current_throttle_value >= self.__throttle_value - self.__change)
-                        and (self.__current_yaw_value <= self.__yaw_value + self.__change and self.__current_yaw_value >= self.__yaw_value - self.__change)
-                        and (self.__current_forward_value <= self.__forward_value + self.__change and self.__current_forward_value >= self.__forward_value - self.__change)
-                        and (self.__current_lateral_value <= self.__lateral_value + self.__change and self.__current_lateral_value >= self.__lateral_value - self.__change)): 
+                        if ((self.__current_roll_value <= self.__roll_value + self.__change_up and self.__current_roll_value >= self.__roll_value - self.__change_up)
+                        and (self.__current_throttle_value <= self.__throttle_value + self.__change_up and self.__current_throttle_value >= self.__throttle_value - self.__change_up)
+                        and (self.__current_yaw_value <= self.__yaw_value + self.__change_up and self.__current_yaw_value >= self.__yaw_value - self.__change_up)
+                        and (self.__current_forward_value <= self.__forward_value + self.__change_up and self.__current_forward_value >= self.__forward_value - self.__change_up)
+                        and (self.__current_lateral_value <= self.__lateral_value + self.__change_up and self.__current_lateral_value >= self.__lateral_value - self.__change_up)): 
                             self.pixhawk.arducopter_disarm()
                     if msg.get_type() == 'HEARTBEAT' and \
                             msg.type != mavutil.mavlink.MAV_TYPE_GCS:
@@ -215,11 +216,11 @@ class Pixhawk(QThread):
                             self.connection_changed.emit(True)
                             print("connected to pixhawk")
 
-                        self.__armed = self.__pixhawk.motors_armed()
-                        if self.__armed != self.sent_armed:
-                            self.sent_armed = self.__armed
-                            self.arm_state_changed.emit(bool(self.__armed))
-                            print("armed" if self.__armed else "disarmed")
+                        self.armed = self.__pixhawk.motors_armed()
+                        if self.armed != self.sent_armed:
+                            self.sent_armed = self.armed
+                            self.arm_state_changed.emit(bool(self.armed))
+                            print("armed" if self.armed else "disarmed")
 
                         self.mode = mavutil.mode_string_v10(msg)
                         if self.mode == "Mode(0x00000000)":
@@ -239,32 +240,33 @@ class Pixhawk(QThread):
                         print("Disconnected, trying to reconnect to the pixhawk...")
 
     def move_rov(self):
-        if self.__armed and self.__connected:
+        if self.armed and self.__connected:
             self.__check_and_correct_movement_values()
             if self.__roll_value > self.__current_roll_value:
-                self.__current_roll_value = min(self.__current_roll_value + self.__change, self.__roll_value)
+                self.__current_roll_value = min(self.__current_roll_value + self.__change_up, self.__roll_value)
             elif self.__roll_value < self.__current_roll_value:
-                self.__current_roll_value = max(self.__current_roll_value - self.__change, self.__roll_value)
+                self.__current_roll_value = max(self.__current_roll_value - self.__change_down, self.__roll_value)
 
             if self.__throttle_value > self.__current_throttle_value:
-                self.__current_throttle_value = min(self.__current_throttle_value + self.__change, self.__throttle_value)
+                self.__current_throttle_value = min(self.__current_throttle_value + self.__change_up, self.__throttle_value)
             elif self.__throttle_value < self.__current_throttle_value:
-                self.__current_throttle_value = max(self.__current_throttle_value - self.__change, self.__throttle_value)
+                self.__current_throttle_value = max(self.__current_throttle_value - self.__change_down, self.__throttle_value)
 
             if self.__yaw_value > self.__current_yaw_value:
-                self.__current_yaw_value = min(self.__current_yaw_value + self.__change, self.__yaw_value)
+                self.__current_yaw_value = min(self.__current_yaw_value + self.__change_up, self.__yaw_value)
             elif self.__yaw_value < self.__current_yaw_value:
-                self.__current_yaw_value = max(self.__current_yaw_value - self.__change, self.__yaw_value)
+                self.__current_yaw_value = max(self.__current_yaw_value - self.__change_down, self.__yaw_value)
 
             if self.__forward_value > self.__current_forward_value:
-                self.__current_forward_value = min(self.__current_forward_value + self.__change, self.__forward_value)
+                self.__current_forward_value = min(self.__current_forward_value + self.__change_up, self.__forward_value)
             elif self.__forward_value < self.__current_forward_value:  # was wrongly decrementing throttle here
-                self.__current_forward_value = max(self.__current_forward_value - self.__change, self.__forward_value)
+                self.__current_forward_value = max(self.__current_forward_value - self.__change_down, self.__forward_value)
 
             if self.__lateral_value > self.__current_lateral_value:
-                self.__current_lateral_value = min(self.__current_lateral_value + self.__change, self.__lateral_value)
+                self.__current_lateral_value = min(self.__current_lateral_value + self.__change_up, self.__lateral_value)
             elif self.__lateral_value < self.__current_lateral_value:
-                self.__current_lateral_value = max(self.__current_lateral_value - self.__change, self.__lateral_value)
+                self.__current_lateral_value = max(self.__current_lateral_value - self.__change_down, self.__lateral_value)
+
             rc_channel_values = [1500, 
                              int(self.__current_roll_value), 
                              int(self.__current_throttle_value), 
@@ -279,7 +281,7 @@ class Pixhawk(QThread):
             self.movements_values_reset()
 
     def track_transect(self, rect_center_x, rect_center_y, rect_angle, frame_width, frame_height):
-        if not self.__armed or not self.__connected:
+        if not self.armed or not self.__connected:
             return
 
         camera_center_x = frame_width / 2
