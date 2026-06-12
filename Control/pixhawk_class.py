0ac7ac6b22ad0c6a692518311666ae26948c378d
Mohamed Ashraf <mtaherr@proton.me>
2026-06-12 15:19:38 +0300

    corrected different ramp speeds logic

--- Control/pixhawk_class.py
+++ Control/pixhawk_class.py
@@ -248,71 +248,66 @@ class Pixhawk(QThread):
     def move_rov(self):
         if self.armed and self.__connected:
             self.__check_and_correct_movement_values()
-            if self.__roll_value > 1550: self.__positive_roll = True
-            elif self.__roll_value < 1450: self.__positive_roll = False
-            if self.__throttle_value > 1550: self.__positive_throttle = True
-            elif self.__throttle_value < 1450: self.__positive_throttle = False
-            if self.__yaw_value > 1550: self.__positive_yaw = True
-            elif self.__yaw_value < 1450: self.__positive_yaw = False
-            if self.__forward_value > 1550: self.__positive_forward = True
-            elif self.__forward_value < 1450: self.__positive_forward = False
-            if self.__lateral_value > 1550: self.__positive_lateral = True
-            elif self.__lateral_value < 1450: self.__positive_lateral = False
 
-            if self.__roll_value > 1500 and self.__positive_roll:
-                if self.__roll_value > self.__current_roll_value:
-                    self.__current_roll_value = min(self.__current_roll_value + self.__change_up, self.__roll_value)
-                elif self.__roll_value < self.__current_roll_value:
-                    self.__current_roll_value = max(self.__current_roll_value - self.__change_down, self.__roll_value)
-            elif self.__roll_value < 1500:
-                if self.__roll_value > self.__current_roll_value:
-                    self.__current_roll_value = min(self.__current_roll_value + self.__change_down, self.__roll_value)
-                elif self.__roll_value < self.__current_roll_value:
-                    self.__current_roll_value = max(self.__current_roll_value - self.__change_up, self.__roll_value)
+            # roll
+            if self.__roll_value > self.__current_roll_value:
+                if self.__current_roll_value == 1500: self.__current_roll_value = 1500 + self.__change_up
+                
+                if self.__current_roll_value > 1500: self.__current_roll_value = min(self.__current_roll_value + self.__change_up, self.__roll_value)
+                elif self.__current_roll_value < 1500: self.__current_roll_value = min(self.__current_roll_value + self.__change_down, self.__roll_value)
+            elif self.__roll_value < self.__current_roll_value:
+                if self.__current_roll_value == 1500: self.__current_roll_value = 1500 - self.__change_up
+                
+                if self.__current_roll_value > 1500: self.__current_roll_value = max(self.__current_roll_value - self.__change_down, self.__roll_value)
+                elif self.__current_roll_value < 1500: self.__current_roll_value = max(self.__current_roll_value - self.__change_up, self.__roll_value)
 
-            if self.__throttle_value > 1500 and self.__positive_throttle:
-                if self.__throttle_value > self.__current_throttle_value:
-                    self.__current_throttle_value = min(self.__current_throttle_value + self.__change_up, self.__throttle_value)
-                elif self.__throttle_value < self.__current_throttle_value:
-                    self.__current_throttle_value = max(self.__current_throttle_value - self.__change_down, self.__throttle_value)
-            elif self.__throttle_value < 1500:
-                if self.__throttle_value > self.__current_throttle_value:
-                    self.__current_throttle_value = min(self.__current_throttle_value + self.__change_down, self.__throttle_value)
-                elif self.__throttle_value < self.__current_throttle_value:
-                    self.__current_throttle_value = max(self.__current_throttle_value - self.__change_up, self.__throttle_value)
+            # throttle
+            if self.__throttle_value > self.__current_throttle_value:
+                if self.__current_throttle_value == 1500: self.__current_throttle_value = 1500 + self.__change_up
 
-            if self.__yaw_value > 1500 and self.__positive_yaw:
-                if self.__yaw_value > self.__current_yaw_value:
-                    self.__current_yaw_value = min(self.__current_yaw_value + self.__change_up, self.__yaw_value)
-                elif self.__yaw_value < self.__current_yaw_value:
-                    self.__current_yaw_value = max(self.__current_yaw_value - self.__change_down, self.__yaw_value)
-            elif self.__yaw_value < 1500:
-                if self.__yaw_value > self.__current_yaw_value:
-                    self.__current_yaw_value = min(self.__current_yaw_value + self.__change_down, self.__yaw_value)
-                elif self.__yaw_value < self.__current_yaw_value:
-                    self.__current_yaw_value = max(self.__current_yaw_value - self.__change_up, self.__yaw_value)
+                if self.__current_throttle_value > 1500: self.__current_throttle_value = min(self.__current_throttle_value + self.__change_up, self.__throttle_value)
+                elif self.__current_throttle_value < 1500: self.__current_throttle_value = min(self.__current_throttle_value + self.__change_down, self.__throttle_value)
+            elif self.__throttle_value < self.__current_throttle_value:
+                if self.__current_throttle_value == 1500: self.__current_throttle_value = 1500 - self.__change_up
 
-            if self.__forward_value > 1500 and self.__positive_forward:
-                if self.__forward_value > self.__current_forward_value:
-                    self.__current_forward_value = min(self.__current_forward_value + self.__change_up, self.__forward_value)
-                elif self.__forward_value < self.__current_forward_value:
-                    self.__current_forward_value = max(self.__current_forward_value - self.__change_down, self.__forward_value)
-            elif self.__forward_value < 1500:
-                if self.__forward_value > self.__current_forward_value:
-                    self.__current_forward_value = min(self.__current_forward_value + self.__change_down, self.__forward_value)
-                elif self.__forward_value < self.__current_forward_value:
-                    self.__current_forward_value = max(self.__current_forward_value - self.__change_up, self.__forward_value)
+                if self.__current_throttle_value > 1500: self.__current_throttle_value = max(self.__current_throttle_value - self.__change_down, self.__throttle_value)
+                elif self.__current_throttle_value < 1500: self.__current_throttle_value = max(self.__current_throttle_value - self.__change_up, self.__throttle_value)
 
-            if self.__lateral_value > 1500 and self.__positive_lateral:
-                if self.__lateral_value > self.__current_lateral_value:
-                    self.__current_lateral_value = min(self.__current_lateral_value + self.__change_up, self.__lateral_value)
-                elif self.__lateral_value < self.__current_lateral_value:
-                    self.__current_lateral_value = max(self.__current_lateral_value - self.__change_down, self.__lateral_value)
-            elif self.__lateral_value < 1500:
-                if self.__lateral_value > self.__current_lateral_value:
-                    self.__current_lateral_value = min(self.__current_lateral_value + self.__change_down, self.__lateral_value)
-                elif self.__lateral_value < self.__current_lateral_value:
-                    self.__current_lateral_value = max(self.__current_lateral_value - self.__change_up, self.__lateral_value)
+            # yaw
+            if self.__yaw_value > self.__current_yaw_value:
+                if self.__current_yaw_value == 1500: self.__current_yaw_value = 1500 + self.__change_up
+
+                if self.__current_yaw_value > 1500: self.__current_yaw_value = min(self.__current_yaw_value + self.__change_up, self.__yaw_value)
+                elif self.__current_yaw_value < 1500: self.__current_yaw_value = min(self.__current_yaw_value + self.__change_down, self.__yaw_value)
+            elif self.__yaw_value < self.__current_yaw_value:
+                if self.__current_yaw_value == 1500: self.__current_yaw_value = 1500 - self.__change_up
+
+                if self.__current_yaw_value > 1500: self.__current_yaw_value = max(self.__current_yaw_value - self.__change_down, self.__yaw_value)
+                elif self.__current_yaw_value < 1500: self.__current_yaw_value = max(self.__current_yaw_value - self.__change_up, self.__yaw_value)
+
+            # forward
+            if self.__forward_value > self.__current_forward_value:
+                if self.__current_forward_value == 1500: self.__current_forward_value = 1500 + self.__change_up
+
+                if self.__current_forward_value > 1500: self.__current_forward_value = min(self.__current_forward_value + self.__change_up, self.__forward_value)
+                elif self.__current_forward_value < 1500: self.__current_forward_value = min(self.__current_forward_value + self.__change_down, self.__forward_value)
+            elif self.__forward_value < self.__current_forward_value:
+                if self.__current_forward_value == 1500: self.__current_forward_value = 1500 - self.__change_up
+
+                if self.__current_forward_value > 1500: self.__current_forward_value = max(self.__current_forward_value - self.__change_down, self.__forward_value)
+                elif self.__current_forward_value < 1500: self.__current_forward_value = max(self.__current_forward_value - self.__change_up, self.__forward_value)
+
+            # lateral
+            if self.__lateral_value > self.__current_lateral_value:
+                if self.__current_lateral_value == 1500: self.__current_lateral_value = 1500 + self.__change_up
+
+                if self.__current_lateral_value > 1500: self.__current_lateral_value = min(self.__current_lateral_value + self.__change_up, self.__lateral_value)
+                elif self.__current_lateral_value < 1500: self.__current_lateral_value = min(self.__current_lateral_value + self.__change_down, self.__lateral_value)
+            elif self.__lateral_value < self.__current_lateral_value:
+                if self.__current_lateral_value == 1500: self.__current_lateral_value = 1500 - self.__change_up
+
+                if self.__current_lateral_value > 1500: self.__current_lateral_value = max(self.__current_lateral_value - self.__change_down, self.__lateral_value)
+                elif self.__current_lateral_value < 1500: self.__current_lateral_value = max(self.__current_lateral_value - self.__change_up, self.__lateral_value)
 
             rc_channel_values = [1500, 
                              int(self.__current_roll_value), 
